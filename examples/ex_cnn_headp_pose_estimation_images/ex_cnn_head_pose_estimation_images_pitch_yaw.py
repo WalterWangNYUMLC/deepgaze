be8574d10b9d684bc8e14cea34e49defb61bc852
WalterWangNYUMLC <mr.walterwang@gmail.com>
2017-07-12 19:31:23 -0400

    changes made

--- examples/ex_cnn_headp_pose_estimation_images/ex_cnn_head_pose_estimation_images_pitch_yaw.py
+++ examples/ex_cnn_headp_pose_estimation_images/ex_cnn_head_pose_estimation_images_pitch_yaw.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
-#The MIT License (MIT)
-#Copyright (c) 2016 Massimiliano Patacchiola
+# The MIT License (MIT)
+# Copyright (c) 2016 Massimiliano Patacchiola
 #
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
-#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import tensorflow as tf
 import cv2
@@ -21,12 +24,12 @@ my_head_pose_estimator = CnnHeadPoseEstimator(sess)  # Head pose estimation obje
 dir_path = os.path.dirname(os.path.realpath(__file__))
 pitchfile_path = os.path.abspath(os.path.join(dir_path, "../../etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"))
 yawfile_path = os.path.abspath(os.path.join(dir_path, "../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k"))
-print(yawfile_path)
+print(pitchfile_path)
 my_head_pose_estimator.load_pitch_variables(pitchfile_path)
 my_head_pose_estimator.load_yaw_variables(yawfile_path)
-image = cv2.imread("1.jpg")  # Read the image with OpenCV
+image = cv2.imread("7.jpg")  # Read the image with OpenCV
 
 pitch = my_head_pose_estimator.return_pitch(image)  # Evaluate the pitch angle using a CNN
 yaw = my_head_pose_estimator.return_yaw(image)  # Evaluate the yaw angle using a CNN
-print("Estimated pitch ..... " + str(pitch[0,0,0]))
-print("Estimated yaw ..... " + str(yaw[0,0,0]))
+print("Estimated pitch ..... " + str(pitch[0, 0, 0]))
+print("Estimated yaw ..... " + str(yaw[0, 0, 0]))
